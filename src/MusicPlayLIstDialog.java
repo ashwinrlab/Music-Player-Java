0980793d22c61c1e38d2035490cc860f818279c5
Ashwin R <ashwinraja1104@gmail.com>
2026-03-17 13:56:13 +0530

    Rename MusicPlayLIstDialog to MusicPlaylistDialog

--- src/MusicPlayLIstDialog.java
+++ src/MusicPlayLIstDialog.java
@@ -3,71 +3,73 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
-public class MusicPlayLIstDialog extends JDialog {
+public class MusicPlaylistDialog extends JDialog {
     private MusicPlayerGUI musicPlayerGUI;
 
-    private ArrayList<String> songpath;
+    // store all of the paths to be written to a txt file (when we load a playlist)
+    private ArrayList<String> songPaths;
 
-    public  MusicPlayLIstDialog(MusicPlayerGUI musicPlayerGUI){
-        this.musicPlayerGUI=musicPlayerGUI;
+    public MusicPlaylistDialog(MusicPlayerGUI musicPlayerGUI){
+        this.musicPlayerGUI = musicPlayerGUI;
+        songPaths = new ArrayList<>();
 
-        songpath = new ArrayList<>();
+        // configure dialog
         setTitle("Create Playlist");
-        setSize(400,400);
+        setSize(400, 400);
         setResizable(false);
         getContentPane().setBackground(MusicPlayerGUI.FRAME_COLOR);
         setLayout(null);
-        setModal(true);
+        setModal(true); // this property makes it so that the dialog has to be closed to give focus
         setLocationRelativeTo(musicPlayerGUI);
 
         addDialogComponents();
-
     }
 
-    private void addDialogComponents() {
-
+    private void addDialogComponents(){
+        // container to hold each song path
         JPanel songContainer = new JPanel();
-        songContainer.setLayout(new BoxLayout(songContainer,BoxLayout.LINE_AXIS));
-        songContainer.setBounds((int)(getWidth()*0.025),10,(int)(getWidth()*0.98),(int) (getHeight()*0.75));
+        songContainer.setLayout(new BoxLayout(songContainer, BoxLayout.Y_AXIS));
+        songContainer.setBounds((int)(getWidth() * 0.025), 10, (int)(getWidth() * 0.90), (int) (getHeight() * 0.75));
         add(songContainer);
 
+        // add song button
         JButton addSongButton = new JButton("Add");
-        addSongButton.setBounds(60,(int)(getHeight()*0.80),100,25);
-        addSongButton.setFont(new Font("Dialog", Font.BOLD,12));
+        addSongButton.setBounds(60, (int) (getHeight() * 0.80), 100, 25);
+        addSongButton.setFont(new Font("Dialog", Font.BOLD, 14));
         addSongButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // open file explorer
                 JFileChooser jFileChooser = new JFileChooser();
-                jFileChooser.setFileFilter(new FileNameExtensionFilter("WAV","wav"));
-                jFileChooser.setFileFilter(new FileNameExtensionFilter("FLAC","flac"));
-                jFileChooser.setFileFilter(new FileNameExtensionFilter("MP3","mp3"));
+                jFileChooser.setFileFilter(new FileNameExtensionFilter("MP3", "mp3"));
                 jFileChooser.setCurrentDirectory(new File("src/assist"));
-                int result = jFileChooser.showOpenDialog(MusicPlayLIstDialog.this);
+                int result = jFileChooser.showOpenDialog(MusicPlaylistDialog.this);
+
                 File selectedFile = jFileChooser.getSelectedFile();
-                if (result == JFileChooser.APPROVE_OPTION && selectedFile!=null){
+                if(result == JFileChooser.APPROVE_OPTION && selectedFile != null){
                     JLabel filePathLabel = new JLabel(selectedFile.getPath());
-                    filePathLabel.setFont(new Font("Dialog",Font.BOLD,12));
+                    filePathLabel.setFont(new Font("Dialog", Font.BOLD, 12));
                     filePathLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
+                    // add to the list
+                    songPaths.add(filePathLabel.getText());
 
-                    songpath.add(filePathLabel.getText());
-
+                    // add to container
                     songContainer.add(filePathLabel);
 
+                    // refreshes dialog to show newly added JLabel
                     songContainer.revalidate();
                 }
             }
         });
-
         add(addSongButton);
 
-
+        // save playlist button
         JButton savePlaylistButton = new JButton("Save");
         savePlaylistButton.setBounds(215, (int) (getHeight() * 0.80), 100, 25);
         savePlaylistButton.setFont(new Font("Dialog", Font.BOLD, 14));
@@ -76,8 +78,8 @@ public class MusicPlayLIstDialog extends JDialog {
             public void actionPerformed(ActionEvent e) {
                 try{
                     JFileChooser jFileChooser = new JFileChooser();
-                    jFileChooser.setCurrentDirectory(new File("src/assets"));
-                    int result = jFileChooser.showSaveDialog(MusicPlayLIstDialog.this);
+                    jFileChooser.setCurrentDirectory(new File("src/assist"));
+                    int result = jFileChooser.showSaveDialog(MusicPlaylistDialog.this);
 
                     if(result == JFileChooser.APPROVE_OPTION){
                         // we use getSelectedFile() to get reference to the file that we are about to save
@@ -98,16 +100,16 @@ public class MusicPlayLIstDialog extends JDialog {
 
                         // iterate through our song paths list and write each string into the file
                         // each song will be written in their own row
-                        for(String songPath : songpath){
+                        for(String songPath : songPaths){
                             bufferedWriter.write(songPath + "\n");
                         }
                         bufferedWriter.close();
 
                         // display success dialog
-                        JOptionPane.showMessageDialog(MusicPlayLIstDialog.this, "Successfully Created Playlist!");
+                        JOptionPane.showMessageDialog(MusicPlaylistDialog.this, "Successfully Created Playlist!");
 
                         // close this dialog
-                        MusicPlayLIstDialog.this.dispose();
+                        MusicPlaylistDialog.this.dispose();
                     }
                 }catch(Exception exception){
                     exception.printStackTrace();
@@ -116,5 +118,12 @@ public class MusicPlayLIstDialog extends JDialog {
         });
         add(savePlaylistButton);
     }
-
 }
+
+
+
+
+
+
+
+
