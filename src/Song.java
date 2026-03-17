6bbaceb6665bc1683e64586d604e6cd97470caa4
Ashwin R <ashwinraja1104@gmail.com>
2026-03-17 13:58:43 +0530

    Refactor Song class for clarity and maintainability
    Updated variable names for clarity and improved comments.

--- src/Song.java
+++ src/Song.java
@@ -6,43 +6,49 @@ import org.jaudiotagger.tag.Tag;
 
 import java.io.File;
 
+// class used to describe a song
 public class Song {
     private String songTitle;
     private String songArtist;
     private String songLength;
     private String filePath;
     private Mp3File mp3File;
-    private double frameRatePerMillisecond;
+    private double frameRatePerMilliseconds;
 
     public Song(String filePath){
-        this.filePath=filePath;
+        this.filePath = filePath;
         try{
             mp3File = new Mp3File(filePath);
-            frameRatePerMillisecond=(double) mp3File.getFrameCount() / mp3File.getLengthInMilliseconds();
-            songLength = convertToSonglengthFormat();
+            frameRatePerMilliseconds = (double) mp3File.getFrameCount() / mp3File.getLengthInMilliseconds();
+            songLength = convertToSongLengthFormat();
 
-            AudioFile audiofile = AudioFileIO.read(new File(filePath));
-            Tag tag = audiofile.getTag();
-            if (tag!=null ){
-                songTitle=tag.getFirst(FieldKey.TITLE);
-                songArtist=tag.getFirst(FieldKey.ARTIST);
+            // use the jaudiotagger library to create an audiofile obj to read mp3 file's information
+            AudioFile audioFile = AudioFileIO.read(new File(filePath));
 
-            }else {
-                songTitle="N/A";
-                songArtist= "N/A";
+            // read through the meta data of the audio file
+            Tag tag =  audioFile.getTag();
+            if(tag != null){
+                songTitle = tag.getFirst(FieldKey.TITLE);
+                songArtist = tag.getFirst(FieldKey.ARTIST);
+            }else{
+                // could not read through mp3 file's meta data
+                songTitle = "N/A";
+                songArtist = "N/A";
             }
-        } catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
-    private  String convertToSonglengthFormat(){
-        long minutes = mp3File.getLengthInSeconds()/60;
-        long seconds = mp3File.getLengthInSeconds()%60;
-        String formattedTime = String.format("%02d:%02d",minutes,seconds);
+
+    private String convertToSongLengthFormat(){
+        long minutes = mp3File.getLengthInSeconds() / 60;
+        long seconds = mp3File.getLengthInSeconds() % 60;
+        String formattedTime = String.format("%02d:%02d", minutes, seconds);
 
         return formattedTime;
     }
 
+    // getters
     public String getSongTitle() {
         return songTitle;
     }
@@ -59,11 +65,21 @@ public class Song {
         return filePath;
     }
 
-    public Mp3File getMp3File(){
-        return mp3File;
-    }
-
-    public double getFrameRatePerMillisecond() {
-        return frameRatePerMillisecond;
-    }
+    public Mp3File getMp3File(){return mp3File;}
+    public double getFrameRatePerMilliseconds(){return frameRatePerMilliseconds;}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
